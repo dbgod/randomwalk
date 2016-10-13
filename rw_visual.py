fe983756cabd9887f5a901c669e546d1c0fb31f1
Chris <christianadams@comcast.net>
2016-10-12 22:28:05 -0400

    Loop random walk
    Wrap random walk code + plot in a loop

--- rw_visual.py
+++ rw_visual.py
@@ -2,8 +2,14 @@ import matplotlib.pyplot as plt
 
 from random_walk import RandomWalk
 
-# Make a random walk, and plot the points.
-rw = RandomWalk()
-rw.fill_walk()
-plt.scatter(rw.x_values, rw.y_valies, s=15)
-plt.show()
+# Make random walks until user quits.
+while True:
+    # Make a random walk, and plot the points.
+    rw = RandomWalk()
+    rw.fill_walk()
+    plt.scatter(rw.x_values, rw.y_valies, s=15)
+    plt.show()
+
+    keep_running = input("Make another walk? (y/n): ")
+    if keep_running == 'n':
+        break
